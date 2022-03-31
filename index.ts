68219f36f7822d521d8d21897edaa7637dd49ab6
Hoa Phan <hoaphantn7604@gmail.com>
2022-03-31 14:02:42 +0700

    update

--- index.ts
+++ index.ts
@@ -1,41 +1,41 @@
 import useScale from './toolkit/ueScale';
 import useDetectDevice from './toolkit/useDetectDevice';
-import useDeviceOrientation from './toolkit/useDeviceOrientation';
+import { useDeviceOrientation } from './toolkit/useDeviceOrientation';
 import useBackHandler from './toolkit/useBackHandler';
 import useAppState from './toolkit/useAppState';
 import useValidation from './toolkit/useValidation';
 import useArray from './toolkit/useArray';
 
-const { 
-    deviceInch, 
-    hasNotch, 
-    height, 
-    isAndroid, 
-    isIOS, 
-    isSmallDevice, 
-    isTablet, 
-    width 
+const {
+    deviceInch,
+    hasNotch,
+    height,
+    isAndroid,
+    isIOS,
+    isSmallDevice,
+    isTablet,
+    width
 } = useDetectDevice;
 
 const { fontScale, scale } = useScale;
 
-export { 
-    useDetectDevice, 
-    useScale, 
-    useDeviceOrientation, 
+export {
+    useDetectDevice,
+    useScale,
+    useDeviceOrientation,
     useBackHandler,
     useAppState,
     useValidation,
     useArray,
-    deviceInch, 
-    hasNotch, 
-    height, 
-    isAndroid, 
-    isIOS, 
-    isSmallDevice, 
-    isTablet, 
-    width, 
-    fontScale, 
+    deviceInch,
+    hasNotch,
+    height,
+    isAndroid,
+    isIOS,
+    isSmallDevice,
+    isTablet,
+    width,
+    fontScale,
     scale,
 
 };
